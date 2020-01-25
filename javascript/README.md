029d79a4f4aac2d1d79da58b39c83d1b228d9c4f
Oli Lalonde <olalonde@gmail.com>
2020-01-25 17:00:37 -0500

    wip

--- javascript/README.md
+++ javascript/README.md
@@ -1,11 +1,19 @@
 # Install
 
 ```
-npm install
+git clone https://github.com/olalonde/phemex-puzzle.git;
+cd javascript;
+npm install;
 ```
 
 # Test
 
 ```
 npm run test:watch
+```
+
+# Run
+
+```
+node approach-1-base58.js
 ```
--- javascript/approach-1-base58.js
+++ javascript/approach-1-base58.js
@@ -6,9 +6,10 @@ const words = ["XRP", "ETH", "BTC", "Phemex"];
 
 const nums = words.map(b58decode);
 const concat = (acc, x) => `${acc}${x}`;
-console.log(words.map(b58decode).reduce(concat, ""));
+console.log(words.reduce(concat, ""));
 const num = BigInt(words.map(b58decode).reduce(concat, ""));
 console.log(countDigits(num));
 verifyNum(num);
 verifyNum(prime21 * num);
 verifyNum(BigInt(`${prime21}${num}`));
+verifyNum(BigInt(`${num}${prime21}`));
