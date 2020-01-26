91566f603b5e12babd9c07269502f7383dacf530
Oli Lalonde <olalonde@gmail.com>
2020-01-26 04:52:31 -0500

    wip

--- javascript/approach-2-bip44.js
+++ javascript/approach-2-bip44.js
@@ -9,6 +9,7 @@ const BTC = 0x80000000n;
 
 console.log(countDigits(XRP)); // 10 digits
 console.log(countDigits(XRP * ETH * BTC)); // 28 digits
+console.log(countDigits(0x800000908000003c80000000n)); // 29 digits
 
 // verifyNum(BigInt(`${prime21}${num}`));
 // verifyNum(BigInt(`${num}${prime21}`));
