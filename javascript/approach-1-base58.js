d6b218d2e0bd6b54d0c9f07967a2094fac093e44
Oli Lalonde <olalonde@gmail.com>
2020-01-25 22:44:33 -0500

    wip

--- javascript/approach-1-base58.js
+++ javascript/approach-1-base58.js
@@ -2,10 +2,10 @@ const { b58decode, countDigits, compose, bn2buf } = require("./utils");
 const { prime21 } = require("./constants");
 const { verifyNum } = require("./verify");
 
+const concat = (acc, x) => `${acc}${x}`;
 const words = ["XRP", "ETH", "BTC", "Phemex"];
 
 let nums = words.map(b58decode);
-const concat = (acc, x) => `${acc}${x}`;
 console.log(words.reduce(concat, ""));
 let num = BigInt(words.map(b58decode).reduce(concat, ""));
 console.log(countDigits(num));
