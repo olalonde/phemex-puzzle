fd2eec14485fe3ff5218e0217b4d76be6c738807
Oli Lalonde <olalonde@gmail.com>
2020-01-27 06:16:45 -0500

    wip

--- javascript/approach-1-base58.js
+++ javascript/approach-1-base58.js
@@ -1,4 +1,4 @@
-const { b58decode, countDigits, bn2buf, buf2bn } = require("./utils");
+const { b58decode, countDigits, bn2buf, buf2bn, prime21 } = require("./utils");
 const { verify27Num } = require("./verify");
 const { powerpermute } = require("./permutations");
 
@@ -36,11 +36,27 @@ const words = ["XRP", "ETH", "BTC", "Phemex"];
     }
   });
 }
+// word wise with % prime21
+{
+  powerpermute(words).forEach(words => {
+    const nums = words.map(b58decode).map(n => n % prime21);
+    let num = BigInt(nums.map(n => `${n}`).join(""));
+    // 27 digits
+    if (countDigits(num) === 27) {
+      verify27Num(num);
+    }
+    num = buf2bn(Buffer.concat(nums.map(bn2buf)));
+
+    if (countDigits(num) === 27) {
+      verify27Num(num);
+    }
+  });
+}
 
 // b)
 {
   num = b58decode("Phemex");
-  console.log(countDigits(num * num * num));
+  console.log(countDigits(num ** 3));
   // 31 digits
 }
 
@@ -50,6 +66,28 @@ const words = ["XRP", "ETH", "BTC", "Phemex"];
   const ETH = b58decode("ETH");
   const BTC = b58decode("BTC");
   const Phemex = b58decode("Phemex");
+
+  // XRP^1 * ETH^2 * BTC^3
+  console.log(countDigits(XRP * ETH * ETH * BTC * BTC * BTC));
+  // 28 digits
+  // XRP^3 * ETH^2 * BTC^1
+  console.log(countDigits(XRP * XRP * XRP * ETH * ETH * BTC));
+  // 29 digits
+  // XRP^1 + ETH^2 + BTC^3
+  console.log(countDigits(XRP + ETH * ETH + BTC * BTC * BTC));
+  // 14 digits
+
+  console.log(countDigits(Phemex * Phemex));
+  // 21 digits
+  console.log(countDigits(Phemex * Phemex * Phemex));
+  // 31 digits
+}
+// c) with % prime21
+{
+  const XRP = b58decode("XRP") % prime21;
+  const ETH = b58decode("ETH") % prime21;
+  const BTC = b58decode("BTC") % prime21;
+  const Phemex = b58decode("Phemex") % prime21;
 
   // XRP^1 * ETH^2 * BTC^3
   console.log(countDigits(XRP * ETH * ETH * BTC * BTC * BTC));
--- javascript/approach-2-bip44.js
+++ javascript/approach-2-bip44.js
@@ -1,24 +1,50 @@
-const { countDigits, b58decode } = require("./utils");
+const { countDigits, b58decode, prime21 } = require("./utils");
 const { verify27Num } = require("./verify");
 
 // const words = ["XRP", "ETH", "BTC", "Phemex"];
 
 // https://github.com/bitcoinjs/bip44-constants/blob/master/index.js
-const XRP = 0x80000090n;
-const ETH = 0x8000003cn;
-const BTC = 0x80000000n;
-const Phemex = b58decode("Phemex");
 
-console.log(countDigits(XRP)); // 10 digits
-console.log(countDigits(XRP * ETH * BTC)); // 28 digits
-console.log(countDigits(0x800000908000003c80000000n)); // 29 digits
-console.log(countDigits(0x800000908000003c80000000n / 21n)); // 28 digits
+{
+  const XRP = 0x80000090n;
+  const ETH = 0x8000003cn;
+  const BTC = 0x80000000n;
+  const Phemex = b58decode("Phemex");
+  console.log(countDigits(XRP)); // 10 digits
+  console.log(countDigits(XRP * ETH * BTC)); // 28 digits
+  console.log(countDigits(0x800000908000003c80000000n)); // 29 digits
+  console.log(countDigits(0x800000908000003c80000000n / 21n)); // 28 digits
 
-console.log(countDigits((XRP * ETH * BTC) / 21n)); // 27 digits
-verify27Num((XRP * ETH * BTC) / 21n);
+  console.log(countDigits((XRP * ETH * BTC) / 21n)); // 27 digits
+  verify27Num((XRP * ETH * BTC) / 21n);
 
-console.log(countDigits(XRP * ETH * BTC - Phemex)); // 28 digits
-console.log(countDigits((XRP ^ ETH ^ BTC) * Phemex)); // 20 digits
+  console.log(countDigits(XRP * ETH * BTC - Phemex)); // 28 digits
+  console.log(countDigits((XRP ^ ETH ^ BTC) * Phemex)); // 20 digits
+}
 
-// verify27Num(BigInt(`${prime21}${num}`));
-// verifyNum(BigInt(`${num}${prime21}`));
+/*
+{
+  const XRP = 0x80000090n;
+  const ETH = 0x8000003cn;
+  const BTC = 0x80000000n;
+  const Phemex = b58decode("Phemex") % prime21;
+  verify27Num((XRP * ETH * BTC) / 21n);
+}
+
+{
+  const XRP = 0x80000090n % prime21;
+  const ETH = 0x8000003cn % prime21;
+  const BTC = 0x80000000n % prime21;
+  const Phemex = b58decode("Phemex");
+  console.log(countDigits(XRP));
+  console.log(countDigits(XRP * ETH * BTC));
+  console.log(countDigits(0x800000908000003c80000000n));
+  console.log(countDigits(0x800000908000003c80000000n / 21n));
+
+  console.log(countDigits((XRP * ETH * BTC) / 21n));
+  verify27Num((XRP * ETH * BTC) / 21n);
+
+  console.log(countDigits(XRP * ETH * BTC - Phemex));
+  console.log(countDigits((XRP ^ ETH ^ BTC) * Phemex));
+}
+*/
