f5c6b622329f965391dc46e7f561b26dc3d5ab6b
Oli Lalonde <olalonde@gmail.com>
2020-02-03 21:30:27 -0400

    wip

--- javascript/dumb.js
+++ javascript/dumb.js
@@ -1,7 +1,16 @@
 const { verify27Num, prime21 } = require("./verify");
+const cliProgress = require("cli-progress");
 
 // all 6 digit numbers
-for (let i = 100000; i < 999999; i++) {
+const start = 100000;
+const end = 999999;
+const total = end - start;
+
+const progress = new cliProgress.SingleBar({});
+progress.start(total, 0);
+
+for (let i = start; i < end; i++) {
+  progress.update(i - start);
   verify27Num(BigInt(`${prime21}${i}`));
   verify27Num(BigInt(`${i}${prime21}`));
 }
